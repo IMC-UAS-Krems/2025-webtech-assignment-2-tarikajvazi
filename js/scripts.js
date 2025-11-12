2a9928c83d2d28bdd392c42a9150fdf295d7aceb
Tarik Ajvazi <tarikajvazi2@gmail.com>
2025-11-12 21:27:48 +0100

    Landing page + add to cart function + dynamic displayng of products

---
+++ js/scripts.js
@@ -0,0 +1,206 @@
+// Products pre-added as a list of objects
+
+const products = [
+    {
+        product_id: 1,
+        product_name: "Mouse",
+        product_type: "Device",
+        product_price: 20,
+        product_image: "./imgs/mouse.png",
+        product_description: "Hardly used device"
+    },
+    {
+        product_id: 2,
+        product_name: "Keyboard",
+        product_type: "Device",
+        product_price: 40,
+        product_image: "./imgs/keyboard.png",
+        product_description: "Developer's best friend"
+    },
+    {
+        product_id: 3,
+        product_name: "Headphones",
+        product_type: "Device",
+        product_price: 35,
+        product_image: "./imgs/headphones.png",
+        product_description: "Great bluetooth headphones"
+    },
+    {
+        product_id: 4,
+        product_name: "Laptop",
+        product_type: "Device",
+        product_price: 300,
+        product_image: "./imgs/laptop.png",
+        product_description: "Newest laptop on the market"
+    },
+    {
+        product_id: 5,
+        product_name: "Coffe",
+        product_type: "Beverage",
+        product_price: 3,
+        product_image: "./imgs/coffee.png",
+        product_description: "A refuel for some, nothing for others"
+    },
+    {
+        product_id: 6,
+        product_name: "Tea",
+        product_type: "Beverage",
+        product_price: 2,
+        product_image: "./imgs/tea.png",
+        product_description: "Great to sip while resting"
+    },
+    {
+        product_id: 7,
+        product_name: "Ice Tea",
+        product_type: "Beverage",
+        product_price: 4,
+        product_image: "./imgs/icetea.png",
+        product_description: "Fuel for creating this website"
+    },
+    {
+        product_id: 8,
+        product_name: "Water",
+        product_type: "Beverage",
+        product_price: 1,
+        product_image: "./imgs/water.png",
+        product_description: "A basic need for every non-/developer"
+    },
+    {
+        product_id: 9,
+        product_name: "Mealbox",
+        product_type: "Essential",
+        product_price: 15,
+        product_image: "./imgs/mealbox.png",
+        product_description: "One meal a day is enough to survive"
+    },
+    {
+        product_id: 10,
+        product_name: "Blanket",
+        product_type: "Essential",
+        product_price: 25,
+        product_image: "./imgs/blanket.png",
+        product_description: "Survive the cold days in/outside"
+    },
+    {
+        product_id: 11,
+        product_name: "Pillow",
+        product_type: "Essential",
+        product_price: 18,
+        product_image: "./imgs/pillow.png",
+        product_description: "Rest your head comfortably"
+    },
+    {
+        product_id: 12,
+        product_name: "Mattress",
+        product_type: "Essential",
+        product_price: 80,
+        product_image: "./imgs/mattress.png",
+        product_description: "This is a luxury only given to senior developers"
+    }
+];
+
+// Filters for products (If we could use jQuery it would be easier (and more beautiful))
+
+// It didnt work without DOMContentLoaded - They need to load before selecting them
+document.addEventListener('DOMContentLoaded', function () {
+
+    // Select the buttons-radios that need to have a onclick
+    // I used addEventListener instead of creating new functions
+    // The logic is to first display all and then hide the needed through a parameter
+
+    const all = document.getElementById("btnradio1");
+    const devices = document.getElementById("btnradio2");
+    const beverages = document.getElementById("btnradio3");
+    const essentials = document.getElementById("btnradio4");
+
+    function hideProducts(className) {
+        const products = document.getElementsByClassName(className);
+        for (let product of products) {
+            product.classList.add("d-none");
+        }
+    }
+
+    function showAllProducts() {
+        const allProducts = document.getElementsByClassName('card');
+        for (let product of allProducts) {
+            product.classList.remove("d-none");
+        }
+    }
+
+    all.addEventListener("click", function () {
+        showAllProducts();
+    });
+
+    devices.addEventListener("click", function () {
+        showAllProducts();
+        hideProducts("Beverage");
+        hideProducts("Essential");
+    });
+
+    beverages.addEventListener("click", function () {
+        showAllProducts();
+        hideProducts("Device");
+        hideProducts("Essential");
+    });
+
+    essentials.addEventListener("click", function () {
+        showAllProducts();
+        hideProducts("Device");
+        hideProducts("Beverage");
+    });
+});
+
+// _____________________________________________________________________
+
+// Add to cart functionalities
+// I am not using ID and instead almost all parameters because it is not a large project
+// So it is easier for me to just loop through the cart array than retreive the information as I do in PHP for example
+
+cart = []
+
+function addToCart(name, type, price, image) {
+    // Check if it is in cart, if yes no need to add other, just increment quantity (default is 1)
+    alreadyInCart = false
+
+    for (let item of cart) {
+        if (item.product_name == name) {
+            item.quantity++
+            alreadyInCart = true
+            console.log("Added +1 to " + item.product_name)
+        }
+    }
+
+    if (!alreadyInCart) {
+        cart.push({
+            product_name: name,
+            product_type: type,
+            product_price: price,
+            product_image: image,
+            quantity: 1
+        })
+    }
+
+    document.getElementById("toast-container").innerHTML += `<div class="toast show bg-success text-white" data-bs-autohide="false" role="alert" aria-live="assertive"
+                aria-atomic="true">
+                <div class="toast-header bg-success text-white">
+                    <strong class="me-auto">HHF - Helpless & Hopeless</strong>
+                    <small class="text-white-50">just now</small>
+                    <button type="button" class="btn-close btn-close-white" data-bs-dismiss="toast"
+                        aria-label="Close"></button>
+                </div>
+                <div class="toast-body">
+                    <b>${name}</b> was added to cart
+                </div>
+            </div>`;
+    
+    // count items dynamically (with length of array it is not possible bcs some product may have quantity 1+)
+
+    item_count = 0;
+    for (let item of cart){
+        item_count += item.quantity;
+    }
+    document.getElementById("items-in-cart").innerHTML = item_count
+
+    console.log(name + " added")
+    console.log(cart)
+}
